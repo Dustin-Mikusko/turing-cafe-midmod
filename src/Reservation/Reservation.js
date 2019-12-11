70fb0dc32e1a0cea888db7342a72d5078834566f
Dustin Mikusko <dustinjm713@yahoo.com>
2019-12-11 12:07:12 -0700

    Add way to grab id from reservation

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Reservation.css'
 
-const Reservation = ({ name, date, time, number}) => {
+const Reservation = ({ id, name, date, time, number, deleteReservation }) => {
   return  (
     <article className="resy-card">
       <h2>{name}</h2>
       <p>{date}</p>
       <p>{time}</p>
       <p>Number of guests: {number}</p>
-      <button type="button" className="cancel-btn">Cancel</button>
+      <button id={id} onClick={deleteReservation} type="button" className="cancel-btn">Cancel</button>
     </article>
   )
 }
