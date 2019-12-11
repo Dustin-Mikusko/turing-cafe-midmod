b83c335fe1315588945e5b33f6486f841c7c5574
Dustin Mikusko <dustinjm713@yahoo.com>
2019-12-11 09:46:49 -0700

    Add componenetDidMount to render fetched reservations

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getReservations, addReservation, removeReservation } from '../apiCalls';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [],
+      error: ''
+    }
+  }
+
+  componentDidMount = () => {
+    getReservations()
+      .then(reservations => this.setState({ reservations }))
+      .catch(err => this.setState({error: err.message}))
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+        
         </div>
         <div className='resy-container'>
-          
+          <Reservations 
+            reservations={this.state.reservations}
+          />
         </div>
       </div>
     )
